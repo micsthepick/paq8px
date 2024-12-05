c6a757129763c7ea7b07665a99fec5d58f178031
micsthepick <11528421+micsthepick@users.noreply.github.com>
2024-12-06 10:13:46 +1100

    timeout on WSL2

--- prep.sh
+++ prep.sh
@@ -3,4 +3,4 @@ if grep -q "tmpfs /mnt/ramdisk tmpfs" /proc/mounts; then
 else
     sudo mount -t tmpfs -o size=2G tmpfs /mnt/ramdisk
 fi
-(cd /sys/devices/system/cpu && echo performance | sudo tee cpu*/cpufreq/scaling_governor)
+(cd /sys/devices/system/cpu && (echo performance | sudo tee cpu*/cpufreq/scaling_governor || echo core | sudo tee /proc/sys/kernel/core_pattern))
